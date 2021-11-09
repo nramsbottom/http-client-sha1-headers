d60902e4952d3a8564aee8f2eadde4cb7aa6adbc
Neil Ramsbottom <nramsbottom@hotmail.com>
2021-11-09 21:52:48 +0100

    Add project files.

---
+++ SignedHttpRequests/AppendSha1HashHeaderHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignedHttpRequests
+{
+    class AppendSha1HashHeaderHandler : DelegatingHandler
+    {
+        private readonly string _secret;
+
+        public AppendSha1HashHeaderHandler(string secret, HttpMessageHandler next) : base(next)
+        {
+            _secret = secret;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.Method != HttpMethod.Post && request.Method != HttpMethod.Put)
+                return await base.SendAsync(request, cancellationToken);
+
+            var body = await request.Content.ReadAsStringAsync(cancellationToken);
+            var hash = Hash(_secret + body);
+
+            request.Headers.Add("X-Signature", hash);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        static string Hash(string input)
+        {
+            using SHA1Managed sha1 = new SHA1Managed();
+            var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var sb = new StringBuilder(hash.Length * 2);
+
+            foreach (var b in hash)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SignedHttpRequests/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SignedHttpRequests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            const string Secret = "abc123";
+            const string Message = "Hello World";
+            const string TargetUrl = "https://neilramsbottom.com";
+
+            var http = new HttpClient(new AppendSha1HashHeaderHandler(Secret, new HttpClientHandler()));
+
+            var request = new HttpRequestMessage(HttpMethod.Post, new Uri(TargetUrl))
+            {
+                Content = new StringContent(Message)
+            };
+
+            var response = await http.SendAsync(request);
+
+            Console.WriteLine((int)response.StatusCode);
+        }
+    }
+}
